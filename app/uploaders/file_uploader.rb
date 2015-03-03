6c7c84e002d4096d03ba13d1bb4ea69a3d5325e1
Calvin Szeto <szeto.calvin@gmail.com>
2015-03-03 00:48:15 -0500

    Change icon size to 300 by 240.

--- app/uploaders/file_uploader.rb
+++ app/uploaders/file_uploader.rb
@@ -36,7 +36,7 @@ class FileUploader < CarrierWave::Uploader::Base
   end
 
   version :icon do
-    process :resize_to_fit => [220, 190]
+    process :resize_to_fit => [300, 240]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
